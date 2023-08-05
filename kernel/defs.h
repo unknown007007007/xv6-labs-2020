1c01ad8b85ee0054c48befc36ec2ee71976d8c3b
unknown007007007 <2440083404@qq.com>
2023-08-05 16:38:53 +0800

    fininsh lab6

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,7 +63,8 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-
+void*           kcopy_n_deref(void *pa); 
+void            krefpage(void *pa); 
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -171,6 +172,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             uvmcheckcowpage(uint64 va);
+int             uvmcowcopy(uint64 va);
 
 // plic.c
 void            plicinit(void);
--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,11 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if((r_scause() == 13 || r_scause() == 15) && uvmcheckcowpage(r_stval())) { // copy-on-write
+    if(uvmcowcopy(r_stval()) == -1){ // 如果内存不足，则杀死进程
+      p->killed = 1;
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
@@ -217,4 +221,3 @@ devintr()
     return 0;
   }
 }
-
